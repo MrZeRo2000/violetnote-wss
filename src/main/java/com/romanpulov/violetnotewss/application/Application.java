a4203af329df26c549afe67844494e8963c085a3
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-01-03 10:31:52 +0200

    minor changes - tomcat path and main app annotation

--- src/main/java/com/romanpulov/violetnotewss/application/Application.java
+++ src/main/java/com/romanpulov/violetnotewss/application/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {
+@SpringBootApplication(scanBasePackages = {
         "com.romanpulov.violetnotewss.controller",
         "com.romanpulov.violetnotewss.services",
         "com.romanpulov.violetnotewss.aspect"
